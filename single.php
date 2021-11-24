99fd60ebaf8e30801a09d2e28280a6becf8d243e
ermiasashebr <56713586+ermiasashebr@users.noreply.github.com>
2021-11-24 10:10:33 +0300

    Add files via upload

---
+++ single.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * The template for displaying all single posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+get_header(); ?>
+
+	<section id="primary" class="content-area col-sm-12 col-lg-8">
+		<div id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', get_post_format() );
+
+			    the_post_navigation();
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+			
+		endwhile; // End of the loop.
+		?>
+		<div class=" row col-sm-12 col-md-12 col-lg-12">
+			<div class="col-lg-5 mb-5">
+				<?php the_field('header_image'); ?>
+			</div>
+			<div class="col-lg-7">
+				<?php the_field('price'); ?>
+				<div class="row">
+					<div class="col-lg bg-info mr-2">
+						<?php the_field('facts'); ?>
+					</div>
+					<div class = "col-lg bg-secondary text-white">
+						<?php the_field('payment_plan'); ?>
+					</div>
+				</div>
+			</div>
+					
+			</div>
+		</div><!-- #main -->
+		
+	</section><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
